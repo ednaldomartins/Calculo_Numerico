c21fe6ffd875164055899b7db6e9ef8ba56769fb
Ednaldo <martinsednaldo@outlook.com>
2019-04-05 18:01:34 -0300

    Remoção de exercícios.

--- lista1_q4.py
+++
@@ -1,6 +0,0 @@
-import math
-
-r = float (input ('digite o tamanho do raio de uma esfera para calcular o tamanho do raio para uma esfera com o volume 40% maior:'))
-volume = ( 4 / 3 ) * ( math.pi*r**3 )
-raio = ( volume * 0.4 * 3 ) / (math.pi * 4 ) 
-print ( raio )
